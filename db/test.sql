3ada359b4ee5697dc3c4f1ccd282b694b8775a78
Joshua Hale <joshuahale829@gmail.com>
2024-02-22 13:07:15 -0600

    Added comments to all applicable files.

--- db/test.sql
+++ db/test.sql
@@ -1,3 +1,9 @@
+-- Purpose: To test the database and see if it is working properly.
+
+-- Install dependencies ("npm i" on command line while in root directory.)
+-- Navigate to db folder, run mySQL (mysql -u root -p), enter password. 
+-- In your command line type "source schema.sql", "source seeds.sql", "source test.sql".
+-- You should see all the data from the databases in your command line. 
 select * from department; 
 select * from roles; 
 select * from employee;
